d51d6f87e07d8f3e1f854e8e5b7fd3828635cf06
DUNCAN BOYD <duncan@wapta.ca>
2025-05-11 17:29:42 -0700

    i2c init (untested)

--- main/includes/main.h
+++ main/includes/main.h
@@ -3,4 +3,22 @@
 
 #include <stdio.h>
 
+#include "driver/i2c_master.h"
+
+static i2c_master_dev_handle_t i2c_dev;
+
+void init_i2c(i2c_master_dev_handle_t* i2c_dev);
+
+// I2C specific
+#define DEFAULT_I2C_SDA_PIN       GPIO_NUM_1
+#define DEFAULT_I2C_SCL_PIN       GPIO_NUM_0
+#define DEFAULT_SCL_SPEED_IN_HZ   (400000)          // 500KHz
+#define DEFAULT_GLITCH_IGNORE_CNT (7)
+#define DEFAULT_XFER_TIMEOUT_MS   (-1)
+#define DEFAULT_I2C_PORT          (0)
+#define DEFAULT_I2C_INTR_PRIORITY (0)
+
+// FM transmitter specific
+#define SI4713_I2C_ADDR           (0x63)
+
 #endif
--- main/src/main.c
+++ main/src/main.c
@@ -4,5 +4,36 @@
 
 void app_main(void)
 {
-    printf("Hello World!\n");
+    printf("Beginning application...\n");
+
+    init_i2c(&i2c_dev);
+
+    printf("Exiting application...\n");
+
+    return;
+}
+
+void init_i2c(i2c_master_dev_handle_t* i2c_dev) {
+
+    printf("Initializing I2C...\n");
+
+    i2c_master_bus_config_t i2c_mst_config = {
+        .clk_source                      = I2C_CLK_SRC_RC_FAST,
+        .i2c_port                        = DEFAULT_I2C_PORT,
+        .sda_io_num                      = DEFAULT_I2C_SDA_PIN,
+        .scl_io_num                      = DEFAULT_I2C_SCL_PIN,
+        .glitch_ignore_cnt               = DEFAULT_GLITCH_IGNORE_CNT,
+        .flags.enable_internal_pullup    = true,
+    };
+    i2c_master_bus_handle_t bus_handle;
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+
+    i2c_device_config_t dev_cfg = {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = SI4713_I2C_ADDR,
+        .scl_speed_hz = DEFAULT_SCL_SPEED_IN_HZ,
+    };
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, i2c_dev));
+
+    return;
 }
